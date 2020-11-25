688825ac9619ff6512677f13ea77059ef5707334
Keita Yamada <keita.n.m.e@gmail.com>
2020-11-25 08:20:23 +0100

    Initial Commit

---
+++ DiaryLesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiaryLesson
+//
+//  Created by Keita Yamada on 2020/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
